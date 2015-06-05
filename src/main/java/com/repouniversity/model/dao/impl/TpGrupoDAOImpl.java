9ebb05427b882ad9239598eaf17975d88ab46e0c
Federico Triay <federico.triay@movile.com>
2015-06-04 22:30:03 -0300

    arreglo los metodos que tiran error

--- src/main/java/com/repouniversity/model/dao/impl/TpGrupoDAOImpl.java
+++ src/main/java/com/repouniversity/model/dao/impl/TpGrupoDAOImpl.java
@@ -3,8 +3,6 @@ package com.repouniversity.model.dao.impl;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,9 +11,6 @@ import org.springframework.stereotype.Repository;
 import com.repouniversity.model.dao.TpGrupoDAO;
 import com.repouniversity.model.dao.query.InsertSQLStatement;
 import com.repouniversity.model.dao.query.SQLStatement;
-import com.repouniversity.model.dao.rowmapper.GrupoRowMapper;
-import com.repouniversity.model.entity.Alumno;
-import com.repouniversity.model.entity.Grupo;
 import com.repouniversity.model.entity.TpGrupo;
 
 @Repository
@@ -23,12 +18,6 @@ public class TpGrupoDAOImpl extends GenericDAOImpl<TpGrupo> implements TpGrupoDA
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
-    @Override
-    public Alumno findAlumnoByGrupoId(Integer grupoId) {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
     @Override
     protected Class<TpGrupo> getEntityClass() {
@@ -60,32 +49,6 @@ public class TpGrupoDAOImpl extends GenericDAOImpl<TpGrupo> implements TpGrupoDA
         return "idtp_grupo";
     }
 
-    @Override
-    public List<Grupo> findGruposByCurso(final Long cursoId) {
-        StringBuilder sql = new StringBuilder();
-
-        sql.append("SELECT DISTINCT g.* ");
-        sql.append("FROM grupo g JOIN alumno_curso ac ON ac.id_grupo = g.id_grupo ");
-        sql.append("WHERE ac.id_curso = ? ");
-        sql.append("AND g.activo = 1");
-
-        List<Grupo> list = doQuery(new SQLStatement(sql.toString()) {
-            @Override
-            public void buildPreparedStatement(PreparedStatement ps) throws SQLException {
-                ps.setLong(1, cursoId);
-            }
-
-            @Override
-            public void doAfterTransaction(int result) {
-            }
-        }, new GrupoRowMapper(), "findCursosForDocenteId: " + cursoId);
-
-        if (list.isEmpty()) {
-            return new ArrayList<Grupo>();
-        }
-
-        return list;
-    }
     @Override
     protected InsertSQLStatement buildInsertSQLStatement(final TpGrupo t) {
         return new InsertSQLStatement("INSERT INTO tp_grupo (id_grupo, id_archivo, descripcion) values (?, ?, ?)") {
