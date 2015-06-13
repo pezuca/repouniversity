07069685a73afe5d5f69524f1db3696e62fe95dc
Federico Triay <federico.triay@movile.com>
2015-06-13 14:26:45 -0300

    arreglo la ruta para bajar los archivos

--- src/main/java/com/repouniversity/web/controller/ArchivoController.java
+++ src/main/java/com/repouniversity/web/controller/ArchivoController.java
@@ -34,7 +34,7 @@ import com.repouniversity.web.exceptions.SubirArchivoException;
 public class ArchivoController {
 
     @Value("${system.fileUpload.location}")
-    private String UPLOAD_PATH = "";
+    private String UPLOAD_PATH;
 
     @Autowired
     private ArchivoService archivoService;
