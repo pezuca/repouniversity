e8b380764060c1533685c12ec1b121d21cf70dce
caeceiis <caeceiis@live.com>
2015-07-09 18:08:25 -0300

    s

--- src/main/java/com/repouniversity/model/dao/rowmapper/VwArchivoRowMapper.java
+++ src/main/java/com/repouniversity/model/dao/rowmapper/VwArchivoRowMapper.java
@@ -16,18 +16,28 @@ public class VwArchivoRowMapper implements RowMapper<VwArchivo> {
         
         vwarchivo.setId(rs.getLong("id_archivo"));
         vwarchivo.setIdTipo(rs.getLong("id_tipo"));
-        vwarchivo.setIdCurso(rs.getLong("id_Curso"));
+        vwarchivo.setIdCurso(rs.getLong("id_curso"));
         vwarchivo.setDescripcion(rs.getString("descripcion"));
         vwarchivo.setNombre(rs.getString("nombre"));
         vwarchivo.setActivo(rs.getBoolean("activo"));
         vwarchivo.setFechasys(rs.getDate("fecsys"));
         vwarchivo.setIdPersona(rs.getLong("persona_id_persona"));
-        // result.setFechaDespublicacion(rs.getDate("fecha_despublicacion"));
-        // result.setFechaPublicacion(rs.getDate("fecha_publicacion"));
+        vwarchivo.setFechaDespublicacion(rs.getDate("fecha_despublicacion"));
+        vwarchivo.setFechaPublicacion(rs.getDate("fecha_publicacion"));
         vwarchivo.setEstado(rs.getLong("estado"));
         vwarchivo.setPath(rs.getString("path"));
         vwarchivo.setTags(rs.getString("tags"));
-        //  result.setGrupo(rs.getLong("grupo"));
+        vwarchivo.setTipoArchivo(rs.getString("tipoArchivo"));
+        vwarchivo.setEstadoArchivo(rs.getString("estadoArchivo"));
+        vwarchivo.setCurso(rs.getString("Curso"));
+        vwarchivo.setDescripcionCurso(rs.getString("descripcionCurso"));
+        vwarchivo.setIdDocente(rs.getLong("id_docente"));
+        vwarchivo.setIdMateria(rs.getLong("id_materia"));
+        vwarchivo.setNombreDocente(rs.getString("nombreDocente"));
+        vwarchivo.setApellidoDocente(rs.getString("apellidoDocente"));
+        vwarchivo.setMateria(rs.getString("materia"));
+
+
         
         return vwarchivo;
     }
